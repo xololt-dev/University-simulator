2fb3e2384f055674b50adcf733341e181782bf9b
FTF <kacper.jachimo@gmail.com>
2022-05-23 19:35:45 +0200

    Update main.cpp

--- OOP-Project/main.cpp
+++ OOP-Project/main.cpp
@@ -6,6 +6,7 @@ mod 7 = day				+			mod 2 = even/odd
 */
 
 #include <iostream>
+#include <conio.h>
 #include "Student.h"
 #include "Professor.h"
 
@@ -13,11 +14,28 @@ int main()
 {
 	Student one;
 	Professor two;
+	unsigned char button;
+	int countDay=0;
+
 
 	one.setSParameters(1,10,0,1,1);
 	two.setPParameters(2, "Janusz", "Brodacz", 10, 2);
 
 	std::cout << one.showSKnowledge() <<"\t"<< two.showPLastName();			//short test
 
+	do
+	{
+		button=getch();
+		if(static_cast <int>(button)==13)
+		{
+			std::cout<<"Day: "<<countDay<<"\n";
+			countDay++;
+		}
+	}while(static_cast <int>(button)!=27);
+
 	return 0;
+}
+void Day()
+{
+	
 }
