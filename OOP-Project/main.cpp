2ca72c2f6431f9e762bbe211f5a171cf65ad62f9
xololt-dev <kuba.lazorko@gmail.com>
2022-05-24 14:19:16 +0200

    Vectors added

--- OOP-Project/main.cpp
+++ OOP-Project/main.cpp
@@ -3,13 +3,15 @@ semester - does week contain only working days?
 professors - in table/vector
 professors - max cap or not?
 mod 7 = day				+			mod 2 = even/odd
+
+many objects with vectors
 */
 
 #include <iostream>
 #include "Student.h"
 #include "Professor.h"
 
-int main()
+int main(int argc, char* argv[])
 {
 	Student one;
 	Professor two;
@@ -18,6 +20,7 @@ int main()
 	two.setPParameters(2, "Janusz", "Brodacz", 10, 2);
 
 	std::cout << one.showSKnowledge() <<"\t"<< two.showPLastName();			//short test
+	std::cout <<"\t" << argc;
 
 	return 0;
 }
