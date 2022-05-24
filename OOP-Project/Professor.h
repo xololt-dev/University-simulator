44f09dca372309cae47dc6151d127d2ecff55660
xololt-dev <kuba.lazorko@gmail.com>
2022-05-24 18:59:05 +0200

    Added examCheck to all Professors, idea of how to manage lectures and exercises dates

--- OOP-Project/Professor.h
+++ OOP-Project/Professor.h
@@ -10,21 +10,25 @@ class Professor
 	std::string lastName;
 	short courseDifficulty;
 	short testAmount;
-
+	bool examCheck;				//Exam
+	//short dayLecture;
+	//char occurrenceLecture;					//O(dd) or E(ven) or B(oth)
+	//short dayExercise[7];
+	//char occurrenceExercise[7];
 public:
-	void setPParameters(int id_, std::string firstName_, std::string lastName_, short courseDifficulty_, short testAmount_);
+	void setPParameters(int id_, std::string firstName_, std::string lastName_, short courseDifficulty_, short testAmount_, bool examCheck_);
 	int showPId();
 	std::string showPFirstName();
 	std::string showPLastName();
 	short showPCourseDifficulty();
 	short showPTestAmount();
+	bool showPExamCheck();
 };
 
 class Strict : public Professor
 {
 	short knowledgeSkillCheck;	//Exercises
 	short knowledgeToGain;		//Lectures
-	bool examCheck;				//Exam
 public:
 	short showPKnowledgeSkillCheck();
 	short showPKnowledgeToGain();
@@ -33,8 +37,7 @@ public:
 class Lenient : public Professor
 {
 	short knowledgeSkillCheck;	//Exercises
-	short knowledgeToGain;		//Lectures
-	bool examCheck;				//Exam
+	short knowledgeToGain;		//Lectures	
 public:
 	short showPKnowledgeSkillCheck();
 	short showPKnowledgeToGain();
