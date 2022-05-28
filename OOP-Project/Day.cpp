8284439165939ab1cfd9bdb616bc180531b2ab98
xololt-dev <kuba.lazorko@gmail.com>
2022-05-28 17:01:56 +0200

    Odd day check added as function. Lectures check updated, ready for next iterations.

--- OOP-Project/Day.cpp
+++ OOP-Project/Day.cpp
@@ -1,28 +1,41 @@
 #include "Day.h"
 #include <math.h>
 
-short Day::checkActivities(short dayNumber_)
+bool isOdd(short dayNumber_)
 {
-	return fmod(dayNumber_, 7);
+	return fmod(ceil(dayNumber_ / static_cast<double>(7)), 2);			//check if week is odd
 }
 
-void Day::getPLessons(std::vector<Professor> &professors_, short weekDay_)
+/*
+function here so that we can understand if the student is going to attend the school today
+if yes, go thru todays activities (getLectures, getExercises)
+otherwise, reduce fatigue
+*/
+
+void getLectures(std::vector<Professor>& professors_, short dayNumber_, bool isOdd_)
 {
+	short weekDay_ = fmod(dayNumber_, 7);
+
 	for (short i = 0; i < professors_.size(); i++)
 	{
 		if (professors_[i].showPDayLecture() == weekDay_)
 		{
 			//there is a lecture from professor i on this day (without odd/even)
-		}
-		if (professors_[i].showPDayExercise(weekDay_))
-		{
-			//there is a exercise (without odd/even)
+			if (!((professors_[i].showPOccurenceLecture() == '0' && !isOdd_) || (professors_[i].showPOccurenceLecture() == 'E' && isOdd_)))				//checking the situations when we don't have a lecture (6 combinations, 2 times where we don't)
+			{
+				/*
+				there is a lecture from professor i on this day (WITH odd/even)			
+				give student exp if attending the lecture
+				*/
+			}
 		}
 	}
 }
 
-void Day::getELessons(std::vector<Academic> &academics_, short weekDay_)
+void getExercises(std::vector<Academic>& academics_, short dayNumber_, bool isOdd_)
 {
+	short weekDay_ = fmod(dayNumber_, 7);
+
 	for (short i = 0; i < academics_.size(); i++)
 	{
 		/*if (academics_[i].showPDayLecture() == weekDay_)
@@ -32,6 +45,9 @@ void Day::getELessons(std::vector<Academic> &academics_, short weekDay_)
 		if (academics_[i].showPDayExercise(weekDay_))
 		{
 			//there is a exercise (without odd/even)
-		}*/
+		}
+		
+		needs to have a check for tests (2 tests equals test in the middle of semester and at the end, three = 1/3 of semester, 2/3 and end etc etc)
+		*/
 	}
 }
