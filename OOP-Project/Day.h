527b38941ff52ea797350aa1f9f1dbd08d84312d
xololt-dev <kuba.lazorko@gmail.com>
2022-05-28 17:00:18 +0200

    Functions no longer tied to Day class.

--- OOP-Project/Day.h
+++ OOP-Project/Day.h
@@ -5,14 +5,8 @@
 #include "Student.h"
 #include <vector>
 
-class Day
-{
-	std::vector<std::vector<int>> weekEven;				// {id(int), type(char)}
-	std::vector<std::vector<int>> weekOdd;
-public:
-	short checkActivities(short dayNumber_);
-	void getPLessons(std::vector<Professor> &professors_, short weekDay_);
-	void getELessons(std::vector<Academic> &academics_, short weekDay_);
-};
+bool isOdd(short dayNumber_);
+void getLectures(std::vector<Professor>& professors_, short dayNumber_, bool isOdd_);
+void getExercises(std::vector<Academic>& academics_, short dayNumber_, bool isOdd_);
 
 #endif
