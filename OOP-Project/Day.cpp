0054616f3ae41bfaea18035bbc32809311d49e38
xololt-dev <kuba.lazorko@gmail.com>
2022-06-06 23:36:21 +0200

    Exams now working

--- OOP-Project/Day.cpp
+++ OOP-Project/Day.cpp
@@ -37,39 +37,72 @@ void updateStatus(std::vector<Professor>& professors_, std::vector<Academic>& ac
 	}
 }
 
-void getLectures(std::vector<Professor>& professors_, std::vector<Student>& students_, short dayNumber_, bool isOdd_)			//searches thru professors to figure out which Lectures happen on that day
+void getLectures(std::vector<Professor>& professors_, std::vector<Student>& students_, short dayNumber_, bool isOdd_, short semesterNumber_)			//searches thru professors to figure out which Lectures happen on that day
 {
 	short weekDay_ = fmod(dayNumber_, 7);
 	short knowledgeAfter = 0;
 	short fatigueAfter = 0;
 
-	for (short i = 0; i < professors_.size(); i++)
+	if(dayNumber_ < 106)
 	{
-		if (professors_[i].lecture.showDay() == weekDay_)
+		for (short i = 0; i < professors_.size(); i++)
 		{
-			//there is a lecture from professor i on this day (without odd/even)
-			if (!((professors_[i].lecture.showOccurence() == '0' && !isOdd_) || (professors_[i].lecture.showOccurence() == 'E' && isOdd_)))				//checking the situations when we don't have a lecture (6 combinations, 2 times where we don't)
+			if (professors_[i].lecture.showDay() == weekDay_)
 			{
-				/*
-				there is a lecture from professor i on this day (WITH odd/even)			
-				give student exp if attending the lecture
-				*/
-				for (short j = 0; j < students_.size(); j++)
+				//there is a lecture from professor i on this day (without odd/even)
+				if (!((professors_[i].lecture.showOccurence() == '0' && !isOdd_) || (professors_[i].lecture.showOccurence() == 'E' && isOdd_)))				//checking the situations when we don't have a lecture (6 combinations, 2 times where we don't)
 				{
-					if (students_[j].showSFatigue() < 89)
+					/*
+					there is a lecture from professor i on this day (WITH odd/even)
+					give student exp if attending the lecture
+					*/
+					for (short j = 0; j < students_.size(); j++)
 					{
-						knowledgeAfter = students_[j].showSKnowledge() + professors_[i].lecture.showKnowledgeToGain();
-						fatigueAfter = students_[j].showSFatigue() + 10;
-						students_[j].setSParameters(students_[j].showSId(), knowledgeAfter, fatigueAfter, students_[j].showSSemester(), students_[j].showSStudying());			//probably need 2 separate functions for this aka WIP
-					}
-					else
-					{
-						knowledgeAfter = students_[j].showSKnowledge() - professors_[i].lecture.showKnowledgeToGain();
-						fatigueAfter = students_[j].showSFatigue() - 10;
-						students_[j].setSParameters(students_[j].showSId(), knowledgeAfter, fatigueAfter, students_[j].showSSemester(), students_[j].showSStudying());
+						if (students_[j].showSStudying())			//checking if student is still studying 
+						{
+							if (students_[j].showSFatigue() < 89)
+							{
+								knowledgeAfter = students_[j].showSKnowledge() + professors_[i].lecture.showKnowledgeToGain();
+								fatigueAfter = students_[j].showSFatigue() + 10;
+								students_[j].setSParameters(students_[j].showSId(), knowledgeAfter, fatigueAfter, students_[j].showSSemester(), students_[j].showSStudying());			//probably need 2 separate functions for this aka WIP
+							}
+							else
+							{
+								knowledgeAfter = students_[j].showSKnowledge() - professors_[i].lecture.showKnowledgeToGain();
+								fatigueAfter = students_[j].showSFatigue() - 10;
+								students_[j].setSParameters(students_[j].showSId(), knowledgeAfter, fatigueAfter, students_[j].showSSemester(), students_[j].showSStudying());
+							}
+						}						
 					}
 				}
 			}
+		}
+	}
+	else if (dayNumber_ == 106)									//if day 106, go thru exams,	seems to be working fine
+	{
+		double examVariable = (14 + 8 * semesterNumber_ - pow(static_cast<double>(semesterNumber_), 2))/(7 * static_cast<double>(semesterNumber_));			//for exam function (difficulty with semsters)
+		double examVariablePow = 0;																															//for pow funct. later
+
+		for (short i = 0; i < professors_.size(); i++)
+		{
+			if (professors_[i].lecture.showLExamCheck())		//does the professor have an exam
+			{
+				if (examVariable > 1)	examVariablePow = 1 - (static_cast<double>(professors_[i].showPCourseDifficulty()) / static_cast<double>(100));			//making sure formula works (at 0.5 point it misbehaves without change)
+				else	examVariablePow = static_cast<double>(professors_[i].showPCourseDifficulty()) / static_cast<double>(100);
+
+				for (short j = 0; j < students_.size(); j++)	//go thru exam for each student
+				{
+					if (students_[j].showSStudying())
+					{
+						double eqVar = examVariable * static_cast<double>(students_[j].showSKnowledge()) / 100 * 7 / static_cast<double>(semesterNumber_);			//if failed exam, end of simulation for that person
+
+						if (!(eqVar > 1))				//if eqVar is above 1, the student has more knowledge than max required for this semester aka automatic pass
+						{
+							if ((1 / (1 + pow(((1 - (eqVar / examVariable)) / eqVar), examVariablePow))) < 0.5000)	students_[j].setSParameters(students_[j].showSId(), students_[j].showSKnowledge(), students_[j].showSFatigue(), students_[j].showSSemester(), 0);
+						}
+					}
+				}
+			}			
 		}
 	}
 }
