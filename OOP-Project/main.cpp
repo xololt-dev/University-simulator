f8d4c89f180dfd49e146f851f247b60ed936a30a
Xololt <105434979+xololt-dev@users.noreply.github.com>
2022-06-14 11:57:08 +0200

    Changed window size, fixed semester iteration bug

--- OOP-Project/main.cpp
+++ OOP-Project/main.cpp
@@ -11,59 +11,77 @@
 #include "Animation.h"
 
 const short windowX = 1024;
-const short windowY = 768;
+const short windowY = 384;
 const short semesterLength = 112;
 const short semesterAmount = 7;
 
-std::vector<Professor> professors;										//vectors to contain Professors, Academics and Students
+std::vector<Professor> professors;																				//vectors to contain Professors, Academics and Students
 std::vector<Academic> academics;
 std::vector<Student> classroom;
-std::vector<Professor> professorsRef;
+std::vector<Professor> professorsRef;																			//reference vectors
 std::vector<Academic> academicsRef;
 std::vector<Student> classroomRef;
 
-std::vector<sf::CircleShape> professorsDisplay;										//vectors to contain displayed objects
+std::vector<sf::CircleShape> professorsDisplay;																	//vectors to contain displayed objects
 std::vector<sf::CircleShape> academicsDisplay;
 std::vector<sf::CircleShape> classroomDisplay;
 std::vector<sf::Text> professorsDisplayText;
 std::vector<sf::Text> academicsDisplayText;
 std::vector<sf::Text> classroomDisplayText;
 
-short dayNumber = 0;												//tracking days passed
+short dayNumber = 0;																							//tracking days passed
 short semesterNumber = 1;
 short simulationNumber = 1;
+
+std::vector<short> simulationParameters;																		/*
+																												short sleep;
+																												short relax;
+																												short study;
+																												short partyFatigue;
+																												short partyKnowledge;
+																												short work;
+																												short lectureFatigue;
+																												short lectureFatigueRecovery;
+																												short exerciseFatigue;
+																												short exerciseFatigueRecovery;
+																												short midSemesterRecovery;
+																												short simulationsToDo;
+																												*/
 
 int main(int argc, char* argv[])
 {
 	unsigned char button;
-	
-	getFromFile(argv[1], professors, academics, classroom);
 
-	professorsRef.assign(professors.begin(), professors.end());
+	simulationParameters.resize(12);
+
+	getObjectsInfo(argv[1], professors, academics, classroom);													//getting info from provided files
+	getSimulationInfo(argv[2], argv[3], simulationParameters);
+
+	professorsRef.assign(professors.begin(), professors.end());													//making sure we have references to run multiple simulations
 	academicsRef.assign(academics.begin(), academics.end());
 	classroomRef.assign(classroom.begin(), classroom.end());
 
-	saveToFile(argv[2], professors, academics, classroom, dayNumber, semesterNumber);
+	saveToFile(argv[3], professors, academics, classroom, dayNumber, semesterNumber);
 
 	if (professors.size() < 11 && academics.size() < 11 && classroom.size() < 57)
 	{
-		sf::RenderWindow window(sf::VideoMode(windowX, windowY), "OOP Project");
+		sf::RenderWindow window(sf::VideoMode(windowX, windowY), "University simulation");
 
 		sf::Font font;
 		font.loadFromFile("LatoWeb-Bold.ttf");
 
-		std::string dayString = std::to_string(dayNumber);
-		std::string semesterString = std::to_string(simulationNumber);
-		std::string simString = std::to_string(simulationNumber);
+		std::string dayString = "Day: " + std::to_string(dayNumber);											//creating simulation information text
+		std::string semesterString = "Semester: " + std::to_string(simulationNumber);
+		std::string simString = "Simulation: " + std::to_string(simulationNumber);
 		sf::Text dayT(dayString, font);
 		sf::Text semesterT(semesterString, font);
 		sf::Text simT(simString, font);
 		dayT.setFillColor(sf::Color::White);
-		dayT.setPosition(412, 512);
+		dayT.setPosition(170, 320);
 		semesterT.setFillColor(sf::Color::White);
-		semesterT.setPosition(512, 512);
+		semesterT.setPosition(432, 320);
 		simT.setFillColor(sf::Color::White);
-		simT.setPosition(612, 512);
+		simT.setPosition(672, 320);
 
 		objectsStudents(classroom, classroomDisplay, classroomDisplayText, semesterNumber, font, windowX, 1);
 
@@ -72,20 +90,20 @@ int main(int argc, char* argv[])
 			sf::Event event;
 			while (window.pollEvent(event))
 			{
-				if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Enter))
+				if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Enter))			//day passes with enter press
 				{
-					getLectures(professors, classroom, dayNumber, isOdd(dayNumber), semesterNumber);
-					getExercises(academics, classroom, dayNumber, isOdd(dayNumber));
+					Day(professors, academics, classroom, dayNumber, isOdd(dayNumber), semesterNumber, simulationParameters);
 					Afterhours(classroom, isWeekend(dayNumber));
-					objectsStudents(classroom, classroomDisplay, classroomDisplayText, semesterNumber, font, windowX, 0);
-					saveToFile(argv[2], professors, academics, classroom, dayNumber, semesterNumber);
 
-					dayString = std::to_string(dayNumber);
+					objectsStudents(classroom, classroomDisplay, classroomDisplayText, semesterNumber, font, windowX, 0);
+					saveToFile(argv[3], professors, academics, classroom, dayNumber, semesterNumber);
+
+					dayString = "Day: " + std::to_string(dayNumber);
 					dayT.setString(dayString);
 					dayNumber++;
 				}
 
-				if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))	event.type = sf::Event::Closed;
+				if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))	event.type = sf::Event::Closed;		//if esc pressed, quit the program
 
 				if (event.type == sf::Event::Closed)
 					window.close();
@@ -96,34 +114,34 @@ int main(int argc, char* argv[])
 			if (dayNumber > semesterLength)
 			{
 				dayNumber = 0;
-				if (semesterNumber < semesterAmount) 		//going between semesters
+				if (semesterNumber < semesterAmount) 															//going between semesters
 				{
 					semesterNumber++;
 					for (short i = 0; i < classroom.size(); i++)
 					{
 						if (classroom[i].showStudying())
 						{
-							classroom[i].updateFatigue(-50);
+							classroom[i].updateFatigue(simulationParameters[10]);													//some recovery between semesters
 							classroom[i].updateSemester();
 						}						
 					}
 				}
-				else if (semesterNumber == semesterAmount && simulationNumber < std::atoi(argv[3]))			//if there are still simulations left
+				else if (semesterNumber == semesterAmount && simulationNumber < simulationParameters[11])				//if there are still simulations left
 				{					
 					simulationNumber++;
 					semesterNumber = 1;
 					
-					semesterString = std::to_string(semesterNumber);
-					semesterT.setString(semesterString);
+					//semesterString = "Semester: " + std::to_string(semesterNumber);
+					//semesterT.setString(semesterString);
 
-					simString = std::to_string(simulationNumber);
+					simString = "Simulation: " + std::to_string(simulationNumber);
 					simT.setString(simString);
 
-					professors.assign(professorsRef.begin(), professorsRef.end());
+					professors.assign(professorsRef.begin(), professorsRef.end());								//we need original values - we get them from reference vectors
 					academics.assign(academicsRef.begin(), academicsRef.end());
 					classroom.assign(classroomRef.begin(), classroomRef.end());
 				}
-				else if (semesterNumber == semesterAmount && simulationNumber == std::atoi(argv[3]))
+				else if (semesterNumber == semesterAmount && simulationNumber == simulationParameters[11])
 				{
 					simulationNumber++;
 				}
@@ -135,17 +153,17 @@ int main(int argc, char* argv[])
 			}
 			else
 			{
-				if (dayNumber == 1 && semesterNumber > 1)
+				if (dayNumber == 1 && semesterNumber > 0)
 				{
-					semesterString = std::to_string(semesterNumber);
+					semesterString = "Semester: " + std::to_string(semesterNumber);
 					semesterT.setString(semesterString);
 				}
-				if (dayNumber == 1 && simulationNumber > std::atoi(argv[3]))
+				if (dayNumber == 1 && simulationNumber > simulationParameters[11])
 				{
 					event.type = sf::Event::Closed;
 					window.close();
 				}
-				for (short i = 0; i < classroom.size(); i++)
+				for (short i = 0; i < classroom.size(); i++)													//drawing everything
 				{
 					window.draw(classroomDisplay[i]);
 					window.draw(classroomDisplayText[i]);
@@ -157,18 +175,6 @@ int main(int argc, char* argv[])
 			window.display();
 		}
 	}
-	/*do
-	{																			//after each day
-		button = _getch();
-		if (static_cast <int>(button) == 13)									//if enter pressed, go to thru the day
-		{
-			getLectures(professors, classroom, dayNumber, isOdd(dayNumber), semesterNumber);
-			getExercises(academics, classroom, dayNumber, isOdd(dayNumber));
-			Afterhours(classroom, isWeekend(dayNumber));
-			saveToFile(argv[2], professors, academics, classroom, dayNumber, semesterNumber);	
-			dayNumber++;
-		}
-	} while (static_cast <int>(button) != 27);									//ESC stops the simulation
-	*/
+
 	return 0;
 }
